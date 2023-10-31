5bba97310e9e29bb8b4bc63513e5e3f315d825dc
Ruby <Hondimanruby@gmail.com>
2023-10-31 00:16:25 -0400

    change framework

---
+++ src/components/Socials.jsx
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react';
+
+import {ImFacebook, ImInstagram} from 'react-icons/im'
+import { CursorContext } from '../context/CursorContext';
+
+const Socials = () => {
+  const {mouseEnterHandler, mouseLeaveHandler} = useContext(CursorContext)
+
+  return (
+  <div 
+  onMouseEnter={mouseEnterHandler}
+  onMouseLeave={mouseLeaveHandler}
+  className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4'>
+      <li>
+        <a href='http://www.facebook.com' target='_blank'>
+          <ImFacebook />
+        </a>
+      </li>
+      <li>
+        <a href='http://www.instagram.com' target='_blank'>
+          <ImInstagram />
+        </a>
+      </li>
+    </ul>
+  </div>
+  );
+};
+
+export default Socials;
